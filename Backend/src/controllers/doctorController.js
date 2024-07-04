d27509367f5826125870bbec3113874145e79c5a
LamTP3 <phuclamtran2810@gmail.com>
2024-07-04 11:28:55 +0700

    #68 learn relationship between model vs database

--- Backend/src/controllers/doctorController.js
+++ Backend/src/controllers/doctorController.js
@@ -40,9 +40,22 @@ let postInforDoctor = async (req, res) => {
     });
   }
 };
+let getDetailDoctorById = async (req, res) => {
+  try {
+    let infor = await doctorService.getDetailDoctorByIdService(req.query.id);
+    return res.status(200).json(infor);
+  } catch (error) {
+    console.log(error);
+    return res.status(200).json({
+      errCode: -1,
+      errMessage: `Error from server...`,
+    });
+  }
+};
 
 module.exports = {
   getTopDoctorHome: getTopDoctorHome,
   getAllDoctors: getAllDoctors,
   postInforDoctor: postInforDoctor,
+  getDetailDoctorById: getDetailDoctorById,
 };
--- Backend/src/services/doctorService.js
+++ Backend/src/services/doctorService.js
@@ -84,8 +84,48 @@ let saveDetailInforDoctor = (inputData) => {
   });
 };
 
+let getDetailDoctorByIdService = (id) => {
+  return new Promise(async (resolve, reject) => {
+    try {
+      if (!id) {
+        resolve({
+          errCode: 1,
+          errMessage: "Missing id!",
+        });
+      } else {
+        let data = await db.User.findOne({
+          where: { id: id },
+          attributes: {
+            exclude: ["password", "image"],
+          },
+          include: [
+            {
+              model: db.Markdown,
+              attributes: ["contentHTML", "contentMarkdown", "description"],
+            },
+            {
+              model: db.Allcode,
+              as: "positionData",
+              attributes: ["valueEn", "valueVi"],
+            },
+          ],
+          raw: true,
+          nest: true,
+        });
+        resolve({
+          errCode: 0,
+          data: data,
+        });
+      }
+    } catch (error) {
+      reject(e);
+    }
+  });
+};
+
 module.exports = {
   getTopDoctorHome: getTopDoctorHome,
   getAllDoctors: getAllDoctors,
   saveDetailInforDoctor: saveDetailInforDoctor,
+  getDetailDoctorByIdService: getDetailDoctorByIdService,
 };
